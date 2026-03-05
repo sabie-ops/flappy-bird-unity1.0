9a7963a0c1e4ac95e8daf9b73d97f3cab52a8b22
Sabie <sabie.niteka@efrei.net>
2026-03-05 12:20:30 +0100

    final

--- Assets/script/pipespawn.cs
+++ Assets/script/pipespawn.cs
@@ -32,7 +32,7 @@ void spawnPipe()
         float highestPoint = transform.position.y + heightOffset;
 
         Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+
     }
 
 }
-
